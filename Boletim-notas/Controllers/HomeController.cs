ebc35ad5ade6da49dba7ce61f63cb8f94db7dacd
RuanLG25 <ruan.l@uni9.edu.br>
2026-06-07 22:36:12 +0000

    fix: corrige erro de sintaxe no HomeController

---
+++ Boletim-notas/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace BoletimNotas.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Boletim()
+        {
+            ViewBag.Disciplinas = new List<string>
+            {
+                "Matemática",
+                "Português",
+                "História",
+                "Geografia",
+                "Ciências"
+                };
+
+            ViewBag.Notas = new List<double>
+            {
+                9.5,
+                7.2,
+                4.8,
+                8.0,
+                6.5
+            };
+
+            return View();
+        }
+    }
+}
---
+++ Boletim-notas/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Boletim_notas.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
